d09433e51af5b9aa46b6d3a31af867d4996c60fd
RaghuSauce <Dantuluri.Raghu@gmail.com>
2018-03-10 21:26:16 -0500

    startng to work with docker

--- RoutingExample/recieve_logs_direct.js
+++ RoutingExample/recieve_logs_direct.js
@@ -18,6 +18,7 @@ amqp.connect('amqp://localhost', function(err, conn) {
         ch.assertQueue('', {exclusive: true}, function(err, q) {
             console.log(' [*] Waiting for logs. To exit press CTRL+C');
 
+
             args.forEach(function(severity) {
                 ch.bindQueue(q.queue, ex, severity);
             });
